7079545421e0446b2ca04f3c71bb03cecc6fee0e
Dallin Sly <krave83@gmail.com>
2023-03-29 16:53:29 -0400

    final check

--- src/components/Card.js
+++ src/components/Card.js
@@ -72,9 +72,8 @@ class Card {
   }
 
   _renderLikes() {
-    if (this._likes.length > 0) {
-      this._likeCounter.textContent = this._likes.length || "";
-    }
+    this._likeCounter.textContent = this._likes.length || "";
+
     if (this.isLiked()) {
       this.element
         .querySelector(".card__like-image")
